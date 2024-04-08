fa6019fe762bc8b20c9861e7570ce288161168dd
orcchieftain <karlos.slaj@gmail.com>
2024-04-08 21:25:05 +0200

    Added error when barking; added dead argument.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+//cargo run "meow" 1> stdout.txt 2> stderr.txt
+//cargo run "woof" 1> stdout.txt 2> stderr.txt
+
 use clap::Parser;
 
 #[derive(Parser)]
@@ -5,15 +8,23 @@ struct Options {
     #[clap(default_value = "Meow!")]
     /// documentation comment (triple slash)
     message: String, // [1]
+    #[clap(default_value = "false", short = 'd', long = "dead")]
+    /// Make the cat appear dead
+    dead: bool,
 }
 
 fn main() {
-    let options = Options::parse(); // [2]
+    let options = Options::parse();
     let message = options.message;
+    let eye = if options.dead { "x" } else { "o" };
     println!("{}", message);
     println!(" \\");
     println!("  \\");
     println!("     /\\_/\\");
-    println!("     ( o o )");
+    println!("     ( {eye} {eye} )");
     println!("      =( I )=");
+
+    if message.to_lowercase() == "woof" {
+        eprintln!("A cat shouldn't bark!")
+    }
 }
